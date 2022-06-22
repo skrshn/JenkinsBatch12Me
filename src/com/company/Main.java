9891f7ea7864071b5ec652e696193a4c59df0c74
Sakir Sahin <skrshn22@gmail.com>
2022-06-21 15:42:03 -1000

    Thirrrrd Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,8 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("asds");
+        System.out.println("asds2222222");
+        System.out.println("asds2222222");
+        System.out.println("asds2222222");
     }
 }
